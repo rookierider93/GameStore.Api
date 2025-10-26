da45adf2a424373fdc2093f210c790aa1e3b7adb
rohan bait <baitrohan1993@outlook.com>
2025-10-26 19:46:46 +0530

    commit staged

--- GameStore.Api/obj/Debug/net9.0/GameStore.Api.AssemblyInfo.cs
+++ GameStore.Api/obj/Debug/net9.0/GameStore.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2e6fb9ab8e8bbbb776d5aae3c02d095d97285b3a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d26ebfa4a48e8fe6ae5cb2efc029d59c230b7aae")]
 [assembly: System.Reflection.AssemblyProductAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
